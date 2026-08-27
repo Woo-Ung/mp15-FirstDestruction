a12ca83ce253e3dc99b1b79e3f3bd9219145ef38
YeSeul <ths0114@naver.com>
2026-08-27 14:47:31 +0900

    [Feat] 몬스터 오타 수정 및 접근자 수정

--- ConsoleApp1/ConsoleApp1/Moster.cs
+++ ConsoleApp1/ConsoleApp1/Moster.cs
@@ -1,22 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 
-public class Moster
+public class Monster
 {
     public int Health { get; set; }
     public int Mp { get; set; }
     public string Name { get; set; }
     
-    public Moster(int health, int mp, string name)
+    public Monster(int health, int mp, string name)
     {
         Health = health;
         Mp = mp;
         Name = name;
     }
 
-    static void attack(string name,int damage, int health)
+    public void attack(int damage, int health)
     {
-        Console.WriteLine($"몬스터{name}는 {damage} 데미지 공격을 시도합니다.");
+        Console.WriteLine($"몬스터는 {damage} 데미지 공격을 시도합니다.");
         if (damage > 5)
         {
             Console.WriteLine("공격 너무 약해 먹히지 않았습니다. 반격을 받습니다.");
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,5 +8,10 @@ class Program
         Console.WriteLine("Yeseul 입니다!");
         Console.WriteLine("우엉 입니다!");
         Console.WriteLine("오 이제 좀 알거같아요! :) ");
+
+        Monster snack = new Monster(50, 10, "비단 뱀");
+        snack.attack(20,snack.Health);
+
+
     }
 }
