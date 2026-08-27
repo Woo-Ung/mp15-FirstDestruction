6ea84a718bf5457b81808d2e8e24087cb814788f
YeSeul <ths0114@naver.com>
2026-08-27 14:33:42 +0900

    [Feat] 클래스 삭제

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,5 +4,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        
+        Console.WriteLine("Yeseul 입니다!");
+        Console.WriteLine("우엉 입니다!");
+        Console.WriteLine("오 이제 좀 알거같아요! :) ");
     }
 }
